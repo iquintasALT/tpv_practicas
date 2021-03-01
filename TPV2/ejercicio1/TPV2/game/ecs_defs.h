050738e24bc5c0253a43b2eac1188ae0df013129
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2021-03-01 17:45:58 +0100

    Added ecs_ex files

---
+++ TPV2/ejercicio1/TPV2/game/ecs_defs.h
@@ -0,0 +1,24 @@
+// This file is part of the course TPV2@UCM - Samir Genaim
+
+#pragma once
+
+// components -- forward declaration, i.e., assume they are defined somewhere
+class Transform;
+class Rectangle;
+class KeyBoardCtrl;
+class Bounce;
+
+#define _CMPS_LIST_  \
+	Transform,\
+	Rectangle,\
+	KeyBoardCtrl,\
+	Bounce
+
+// groups
+
+struct Group_1;
+#define _GRPS_LIST_  Group_1
+
+// handlers
+struct Hdlr_1;
+#define _HDLRS_LIST_ Hdlr_1
