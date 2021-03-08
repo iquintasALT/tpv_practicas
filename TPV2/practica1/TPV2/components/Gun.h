ba9629f11f7990b08f962e2c4785dedde6bcfcdc
iquintasALT <iquintas@ucm.es>
2021-03-08 17:40:38 +0100

    gun sin parametro manager

--- TPV2/practica1/TPV2/components/Gun.h
+++ TPV2/practica1/TPV2/components/Gun.h
@@ -16,7 +16,7 @@ class Gun : public Component
 {
 public:
 	Gun(Manager* mngr) :
-		tr_(nullptr), mngr_(mngr) {};
+		tr_(nullptr) {};
 
 	virtual ~Gun() {
 	}
@@ -37,7 +37,7 @@ public:
 	}
 
 	void createBullet() {
-		auto bullet = mngr_->addEntity();
+		auto bullet = entity_->getMngr()->addEntity();
 
 		Vector2D bPos = (tr_->getPos() + Vector2D(tr_->getW() / 2, tr_->getH() / 2)) + Vector2D(bulletW / 2.0f, bulletH / 2.0f) -
 			Vector2D(0.0f, bulletH / 2.0f + 5.0f + 12.0f).rotate(tr_->getRot()) - Vector2D(2.0f, 10.0f);
@@ -51,7 +51,6 @@ public:
 
 private:
 	Transform* tr_;
-	Manager* mngr_;
 	const int bulletH = 20.0f;
 	const int bulletW = 5.0f;
 	int msToNextBullet = 0;
