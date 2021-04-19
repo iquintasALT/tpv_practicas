ef4347b3bf87d75a0475f1305eb6c6c04d400f92
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2021-04-19 19:22:34 +0200

    Añadida estructura basica de ecs con sistemas

---
+++ TPV2/practica2/TPV2/game/ecs_defs.h
@@ -0,0 +1,56 @@
+// This file is part of the course TPV2@UCM - Samir Genaim
+
+#pragma once
+
+// components -- forward declaration, i.e., assume they are defined somewhere
+class Transform;
+class FighterCtrl;
+class Image;
+class DeAcceleration;
+class Heart;
+class ShowAtOppositeSide;
+class Generations;
+class DisableOnExit;
+class Gun;
+class FramedImage;
+class Follow;
+class State;
+class GameCtrl;
+class AsteroidsManager;
+class CollisionsManager;
+
+#define _CMPS_LIST_  \
+	Transform,\
+	FighterCtrl,\
+	Image,\
+	DeAcceleration, \
+	Heart, \
+	ShowAtOppositeSide, \
+	Generations, \
+	DisableOnExit, \
+	Gun, \
+	FramedImage, \
+	Follow, \
+	State, \
+	GameCtrl, \
+	AsteroidsManager, \
+	CollisionsManager
+
+// groups
+struct Asteroid_grp;
+struct Bullet_grp;
+#define _GRPS_LIST_  Asteroid_grp, \
+	Bullet_grp
+
+// handlers
+struct Player_hdlr;
+struct Manager_hdlr;
+#define _HDLRS_LIST_ Player_hdlr, \
+	Manager_hdlr
+
+
+// systems
+struct AsteroidSystem;
+struct RenderSystem;
+#define _SYS_LIST_ AsteroidSystem, \
+	RenderSystem
--- TPV2/practica2/TPV2/main.cpp
+++ TPV2/practica2/TPV2/main.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
-#include <SDL.h>
+#include <memory>
 
-#include "sdlutils/sdlutils_demo.h"
+#include "./game/Game.h"
 
-int main(int ac, char **av) {
+void start() {
+	Game g;
 
-	sdlutils_basic_demo();
+	g.init();
+	g.start();
+}
+
+
+int main(int x, char** args) {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	try {
+		start();
+	}
+	catch (const std::string& e) { // catch exceptions thrown as strings
+		std::cerr << e << std::endl;
+	}
+	catch (const char* e) { // catch exceptions thrown as char*
+		std::cerr << e << std::endl;
+	}
+	catch (const std::exception& e) { // catch exceptions thrown as a sub-type of std::exception
+		std::cerr << e.what();
+	}
+	catch (...) {
+		std::cerr << "Caught and exception of unknown type ...";
+	}
 
 	return 0;
 }
-
